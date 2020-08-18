ceefc33a1e9eb1ca04e32fbd2ce05bd034051434
daniimartin <68691828+daniimartin@users.noreply.github.com>
2020-08-18 11:51:15 +0200

    ProjectMovies
    Project on a movie distributor, with Login, using C #, .net with MVC, SQL Server and Session to store the user

---
+++ ProyectoPeliculas/ProyectoPeliculas/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoPeliculas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            if(Session["USUARIO"] != null)
+            {
+                ViewBag.Mensaje = "Bienvenido, ";
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+
+        //GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //POST: Login
+        [HttpPost]
+        public ActionResult Login(String usuario, String password)
+        {
+
+            if(usuario == "dani" && password == "dani")
+            {
+                //Almacenamos el usuario en la sesión (En verdad tendríamos una clase con los datos del usuario y guardaríamos eso)
+                Session["USUARIO"] = usuario;
+                ViewBag.Mensaje = "Bienvenido, " + usuario;
+                
+                return View();
+            }
+            else
+            {
+                ViewBag.Mensaje = "Usuario incorrecto";
+                return View();
+            }
+            
+        }
+    }
+}
